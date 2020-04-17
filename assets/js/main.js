f77c6c455e6f2344e3d138a27f4992810eac9a8a
heshamabdulzaher <heshamabdulzaher@gmail.com>
2020-04-17 20:25:48 +0200

    Increase hero img height, update bg-gradient for icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,18 @@ document.addEventListener('scroll', function (e) {
     header.classList.remove('show');
   }
 });
+
+// Look at the competition
+window.onload = function () {
+  collapseAmazonCard();
+};
+window.onresize = function () {
+  collapseAmazonCard();
+};
+
+const amazonCard = document.querySelector('.comparing-cards .card.amazon');
+function collapseAmazonCard() {
+  if (window.innerWidth <= 1000) {
+    amazonCard.classList.add('collapse');
+  }
+}
