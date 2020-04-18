16f85afd0cab368c1abc796e908b01362ae8c62d
heshamabdulzaher <heshamabdulzaher@gmail.com>
2020-04-18 02:30:44 +0200

    Full Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,61 @@ window.onresize = function () {
   collapseAmazonCard();
 };
 
+const comaringCardsContainer = document.querySelector('.comparing-cards');
 const amazonCard = document.querySelector('.comparing-cards .card.amazon');
+const alibabaCard = document.querySelector('.comparing-cards .card.alibaba');
+const nextBtn = document.querySelector('.comparing-cards .next');
+const prevBtn = document.querySelector('.comparing-cards .prev');
+prevBtn.addEventListener('click', handleShowingComparingCards);
+nextBtn.addEventListener('click', handleShowingComparingCards);
 function collapseAmazonCard() {
-  if (window.innerWidth <= 1000) {
+  if (window.innerWidth <= 1000 && window.innerWidth > 680) {
     amazonCard.classList.add('collapse');
+    nextBtn.classList.add('show');
+  } else if (window.innerWidth <= 680) {
+    alibabaCard.classList.add('collapse');
+    amazonCard.classList.add('collapse');
+    prevBtn.classList.add('show');
+    nextBtn.classList.add('show');
+  }
+}
+
+function handleShowingComparingCards(e) {
+  if (window.innerWidth <= 1000 && window.innerWidth > 680) {
+    amazonCard.classList.toggle('collapse');
+    alibabaCard.classList.toggle('collapse');
+    prevBtn.classList.toggle('show');
+    nextBtn.classList.toggle('show');
+  } else if (window.innerWidth <= 680) {
+    let currentActiveCard = document.querySelector(
+      '.comparing-cards .card:not(.collapse)'
+    );
+    let currentIndex = parseInt(currentActiveCard.dataset.cardIndex);
+    let nextIndex, nextCard;
+    let arrow = e.target.closest('.arrow');
+    if (arrow.classList.contains('next')) {
+      nextIndex = currentIndex + 1;
+      if (nextIndex === 3) {
+        prevBtn.classList.add('show');
+        nextBtn.classList.remove('show');
+      } else {
+        prevBtn.classList.add('show');
+        nextBtn.classList.add('show');
+      }
+    } else {
+      nextIndex = currentIndex - 1;
+      if (nextIndex === 1) {
+        nextBtn.classList.add('show');
+        prevBtn.classList.remove('show');
+      } else {
+        prevBtn.classList.add('show');
+        nextBtn.classList.add('show');
+      }
+    }
+    nextCard = comaringCardsContainer.querySelector(
+      `.card[data-card-index="${nextIndex}"]`
+    );
+    currentActiveCard.classList.add('collapse');
+    nextCard.classList.remove('collapse');
   }
 }
